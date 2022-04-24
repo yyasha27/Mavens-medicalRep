62569ab12b0560db75d3b0e39c2f6336f46d498c
Yashawini S <yashawini.sugumar27@gmail.com>
2022-04-24 10:59:12 +0530

    second commit

---
+++ src/main/java/com/example/scheduler/medicalscheduler/service/EventSchedulerServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.scheduler.medicalscheduler.service;
+
+public class EventSchedulerServiceImpl {
+    
+}
---
+++ src/main/java/com/example/scheduler/medicalscheduler/service/EventSchedulerservice.java
@@ -0,0 +1,12 @@
+package com.example.scheduler.medicalscheduler.service;
+
+import java.util.List;
+
+import com.example.scheduler.medicalscheduler.model.EventSchedulermodel;
+
+public interface EventSchedulerservice {
+    public EventSchedulermodel schedule(EventSchedulermodel details);
+    public List<EventSchedulermodel> getscheduledetails();
+
+    
+}
